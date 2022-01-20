431bc7ce9d453ac6a282f16a95ff6c39342698ba
alex <cuatroalejandro@gmail.com>
2022-01-19 23:25:46 -0400

    clean gym environment  setup

--- RobotEnv/__init__.py
+++ RobotEnv/__init__.py
@@ -1,6 +1,1 @@
-from gym.envs.registration import register
 
-register(
-    id='UR5_EnvTest-v0',
-    entry_point='RobotEnv.envs:UR5_EnvTest'
-)
--- RobotEnv/envs/__init__.py
+++
@@ -1,1 +0,0 @@
-from RobotEnv.envs.UR5_Env import UR5_EnvTest
