5d8cda04c53a3ef30984ad7027b0b3c9af42db66
alex <cuatroalejandro@gmail.com>
2022-02-21 14:15:33 -0400

    more fixes

--- RobotEnv/envs/UR5_Env.py
+++ RobotEnv/envs/UR5_Env.py
@@ -6,7 +6,7 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 
 class UR5_EnvTest(gym.Env):
-    def __init__(self, model, simulation_frames, torque_control, distance_threshold, Gui):
+    def __init__(self, simulation_frames, torque_control, distance_threshold, Gui, model):
         """
         argumentos:
             rewarded_distance: distancia recompenzada cuando te acercas a la distancia target
