84061d2225da71863a9e375dfc35525e5eb5740e
alex <cuatroalejandro@gmail.com>
2022-02-25 23:41:48 -0400

    add memory and fix policy sizes

--- RobotEnv/algorithms/vpg.py
+++ RobotEnv/algorithms/vpg.py
@@ -16,6 +16,11 @@ def mlp(sizes, activation, output_activation=nn.Identity):
     
     return nn.Sequential(*layers)
 
+# combinar dimensiones
+def combined_shape(length, shape=None):
+    if shape in None:
+        return (length,)
+    return (length, shape) if np.isscalar(shape) else (length, *shape)
 
 # politica de control
 class Policy(nn.Module):
@@ -49,13 +54,69 @@ class Policy(nn.Module):
 
         return pi, logp_a
 
+# colección de trayectorias D_k = {tau_i} tambien conocido como buffer
+class memory:
+    """
+    Esta es la memoria del agente que almacena las trayectorias experimentadas con el ambiente
+    usando Generalized Advantage Estimation para calcular los advantages de un par acción estado
+    """
+    def __init__(self,  obs_dim, act_dim, size, gamma=0.99, lam=0.95):
+        self.obs_memory = np.zeros(core.combined_shape(size, obs_dim), dtype=np.float32)
+        self.act_memory = np.zeros(core.combined_shape(size, act_dim), dtype=np.float32)
+        self.advantage_memory = np.zeros(size, dtype=np.float32)
+        self.reward_memory = np.zeros(size, dtype=np.float32)
+        self.j_memory = np.zeros(size, dtype=np.float32)
+        self.v_memory = np.zeros(size, dtype=np.float32)
+        self.logp_memory = np.zeros(size, dtype=np.float32)
+        self.gamma, self.lam = gamma, lam
+        self.ptr, self.path_start_idx, self.max_size = 0, 0, size
+
+    def store_data(self, obs, act, reward, v, logp):
+        """
+        agrega un paso de interacción con el ambiente a la memoria
+        """
+        assert self.ptr < self.max_size
+        self.obs_memory[self.ptr] = obs
+        self.act_memory[self.ptr] = act
+        self.reward_memory[self.ptr] = rew
+        self.v_memory[self.ptr] = v
+        self.logp_memory[self.ptr] = logp
+        self.ptr += 1
+    
+    # TODO: terminar y quitar el uso de MPI
+    def get_data(self):
+        """
+        esta función te devuelve toda la data de la memoria.
+        se llama al  final  del episodio de entrenamiento
+        ya se encuentra con los advantages normalizados
+        """
+        # chequear que la memoria esta full
+        assert self.ptr == self.max_size
+        self.ptr, self.path_start_idx = 0, 0
+
+        # implemento el advantage normalization
+        advantage_memory = np.array(self.advantage_memory, dtype=np.float32) #x
+        global_sum,  global_n = 
+        advantge_mean = global_sum / global_n
+
+        global_sum_sq = 
+        advantage_std = np.sqrt(global_sum_sq / global_n) #computar global std
+
+        self.advantage_memory = (self.advantage_memory - advantage_mean) / advantage_std
+        data = dict(obs=self.obs_memory, action=self.act_memory, j=self.j_memory,
+                    advantage=self.advantage_memory, logp=self.logp_memory)
+
+        return {k: torch.as_tensor(v, dtype=torch.float32) for k,v in data.items()}
+
+
+
 
 # función valor
 class Value_function(nn.Module):
     
     def __init__(self, obs_dim,hidden_sizes, activation):
         super().__init__()
-        self.value_network = mpl([obs_dim] + list(hidden_sizes) + [1], activation)
+        self.value_network = mlp([obs_dim] + list(hidden_sizes) + [1], activation)
 
     def forward(self, obs):
         return torch.squeeze(self.value_network(obs), -1) # para asegurar que v tiene la forma correct
@@ -63,9 +124,12 @@ class Value_function(nn.Module):
 if __name__ == "__main__":
 
     env = UR5_EnvTest(simulation_frames=10, torque_control= 0.01, distance_threshold=0.5, Gui=False)
-    obs_dim = env.observation_space.shape
-    act_dim = env.action_space.shape
+    obs_dim = env.observation_space.shape[0]
+    act_dim = env.action_space.shape[0]
 
 
-    policy = Policy(obs_dim, act_dim, (64,64), activation=nn.Tanh)
-    print(policy)
+    pi = Policy(obs_dim, act_dim, (64,64), activation=nn.Tanh)
+    v = Value_function(obs_dim, hidden_sizes=(64,64), activation=nn.Tanh)
+    
+    print(pi)
+    print(v)
