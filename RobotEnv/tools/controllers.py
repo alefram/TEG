1f0721f5e2004891326d80b71dc5506912232337
alex <cuatroalejandro@gmail.com>
2022-04-27 21:02:58 -0400

    update inverse kinematics

--- RobotEnv/tools/controllers.py
+++ RobotEnv/tools/controllers.py
@@ -172,11 +172,57 @@ class Mujoco_controller(object):
         self.control_list.append(PID(Kp=0.5, Ki=0.0, Kd=0.0, sample_time=sample_time)) #wrist3        
 
     #TODO: terminar cinematica inversa para obtener posiciones de los joints
-    def inverse_kinematics(self, target=None ):
+    def inverse_kinematics(self, target_pos=None, target_quad=None, tol=1e-6, stop_steps=100):
         """cinematica inversa"""
 
-        return q_reference
-    
+        success = False
+
+        # paso 1 obtener posicion y la matriz de rotacion de el link base
+        base_pos = self.sim.data.get_body_xpos("base_link")        
+        base_quad = self.sim.data.get_body_xquat("base_link")
+
+        # paso 2 obtener la posicion del target_link
+        target_pos = self.sim.data.get_body_xpos("wrist_3_link")
+        target_quad = self.sim.data.get_body_xquat("wrist_3_link")
+
+        #paso 3 definir el vector q que tiene los angulos desde la base hasta el target
+        current_q = self.sim.data.qpos.flat.copy()
+
+
+        for steps in range(stop_steps):
+            
+            #paso 4 calculate with foward kinematics with mujoco
+            
+        
+
+            #step 5 calculate error in position and attitude
+            if target is not None:
+                delta_pos = target_pos - current_target_pos
+                delta_pos_norm += np.linalg.norm(delta_pos)
+            
+            if target_quad is not None:
+                #TODO: hacer error rotacional
+
+            
+            #paso 6 si es tolerable entonces terminar
+            if delta_pos < tol:
+                print("Convergio")
+                success = True
+                break
+
+            #paso 7 si no es tolerable entonces calcular delta_q 
+            else:
+                #TODO: calcular el delta_q
+
+
+            
+            
+            #paso 8 actualizar los angulos de los joints como q = q + delta_q
+            current_q = current_q + delta_q
+
+        return current_q, success
+
+            
     def move_to(self, target): 
         """mover la posición de la garra hacia el target"""
 
