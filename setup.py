8be97a5da77948aede08d6a02aad0342e015dbcd
Alexfm101 <fraumalex@gmail.com>
2022-12-02 23:09:15 -0400

    clean a lot

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='RobotEnv',
+    name='TEG',
     version='0.0.1',
     description='Environments and tools for develop smart controllers',
     author='Alexis Fraudita',
@@ -10,7 +10,7 @@ setup(
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: Apache  Software License'
     ],
-    install_requires=['gym', 'mujoco_py'],
+    install_requires=[],
     python_requires='>=3.7',
     py_modules=[]
 )
